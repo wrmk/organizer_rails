2a1f06dff1bbe8e41a389ca0658050e8ffefc8a9
wrmk <elmakan@gmail.com>
2021-06-27 16:57:08 +0300

    create todos controller

--- app/controllers/todos_controller.rb
+++ app/controllers/todos_controller.rb
@@ -1,30 +1,36 @@
 class TodosController < ApplicationController
   
-  # def show
-  #   set_todo
-  #   render json: @todo
-  # end
-
   def update
-    set_todo
 
+    @project = Project.find(params[:project_id])
+    @todo = @project.todos.find(params[:id])
     @todo.update(todo_params)
 
   end  
 
+  def create
 
+    @project = Project.all
+    if @project.where(project_params).exists?
+      @project=Project.find_by project_params
+      @project.todos.create(todo_params)
+    else
+      @project = Project.create(project_params)
+      @project.todos.create(todo_params)
+    end
+
+  end
 
 
 
   private
-    
-    def set_todo
-    @project = Project.find(params[:project_id])
-    @todo = @project.todos.find(params[:id])
-    end
-
-    
+        
     def todo_params
-      params.permit(:isCompleted)
-    end  
+      params.permit(:text, :isCompleted)
+    end 
+
+    def project_params
+      params.permit(:title)
+    end 
+
 end
