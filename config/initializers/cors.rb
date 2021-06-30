3c0f1eba728e55d9acd54e4a6e4f225782ee25f7
wrmk <elmakan@gmail.com>
2021-06-30 21:36:26 +0300

    gem cors

---
+++ config/initializers/cors.rb
@@ -0,0 +1,9 @@
+Rails.application.config.middleware.insert_before 0, Rack::Cors do
+  allow do
+    origins 'http://localhost:4200'
+
+    resource '*',
+      headers: :any,
+      methods: [:get]
+  end
+end
