6286db89352fb3a724159dac2bdb55df749d166a
wrmk <elmakan@gmail.com>
2021-07-05 11:26:08 +0300

    Update cors.rb

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -1,6 +1,6 @@
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins 'http://localhost:4200', 'https://ng-project.herokuapp.com/',
+    origins 'http://localhost:4200', 'https://ng-project.herokuapp.com/'
 
     resource '*',
       headers: :any,
