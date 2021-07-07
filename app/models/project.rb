416c970114b76b45d028e0854ef0e90d1c3062c4
wrmk <elmakan@gmail.com>
2021-07-07 18:11:48 +0300

    Update project.rb

--- app/models/project.rb
+++ app/models/project.rb
@@ -1,5 +1,6 @@
 class Project < ApplicationRecord
-  validates :title, uniqueness: {message: "that type of todo is already exist"}
+  validates :title, presence: true, uniqueness: {message: "that type of todo is already exist"}
 
   has_many :todos, -> {order('id ASC')}
+  accepts_nested_attributes_for :todos
 end
